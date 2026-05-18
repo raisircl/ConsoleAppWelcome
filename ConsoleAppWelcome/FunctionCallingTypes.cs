ea53e52bae963609925cbebfe4cb6b571c182c2a
raisircl <rai.verma@live.com>
2026-05-18 19:32:01 +0530

    functions complete

---
+++ ConsoleAppWelcome/FunctionCallingTypes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppWelcome
+{
+    // call by value
+    // call by reference
+    // call by out
+    internal class FunctionCallingTypes
+    {
+        static void Main()
+        {
+            int a = 10;
+            Console.WriteLine($"Before call value of A={a}");
+            //fun(a); // a is actual parameter and x is formal parameter
+            fun2(ref a); // a is actual parameter and x is formal parameter 
+            // in call by ref if any change in formal parameter also affect actual parameter because both are same memory location but in call by value if any change in formal parameter does not affect actual parameter
+            // in call by value if any change in formal parameter does not affect actual parameter
+            Console.WriteLine($"After call value of A={a}");
+
+            int h = 80, e = 90, m = 95, sci = 85, sst = 75, t=0;
+            double p;
+            result(h, e, m, sci, sst, out t, out p);
+            Console.WriteLine($"Total marks is {t} and percentage is {p}");
+        }
+        static void fun(int x) // value of a is copied to x and x is formal parameter and a is actual parameter
+        {
+            x = x + 10; // value of x is changed but value of a is not changed
+            Console.WriteLine($"Value of x inside fun function is {x}");
+        }
+        static void fun2(ref int x)
+        {
+            x = x + 10; // value of x is changed but value of a will changed
+            Console.WriteLine($"Value of x inside fun function is {x}");
+
+        }
+        static void result(int h, int e, int m, int sci, int sst, out int total, out double per)
+        {
+            
+            total = h + e + m + sci + sst;
+            per = total / 5.0;
+        }
+    }
+}
---
+++ ConsoleAppWelcome/MyFunctions.cs
@@ -0,0 +1,65 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace ConsoleAppWelcome
+//{
+//    internal class MyFunctions
+//    {
+//        static void Main() // calling function
+//        {
+//            Console.WriteLine("Welcome to Main");
+//            Msg(); // calling 
+//            Console.WriteLine("Welcome to Main again");
+//            table(10);
+//            int x = Factorial(5);
+//            Console.Write($"\b={x}");
+
+//            double r, area;
+//            r = 5.3;
+//            area=GetPi() * r * r;
+//            Console.WriteLine($"\nArea of circle with radius {r} is {area}");
+//        }
+//        static void Msg() // called function    
+//        {
+//            Console.WriteLine("I am a function does not accept and return");
+//        }
+//        static void table(int num) // does not return anything but accept parameter type int
+//        {
+//            int i;
+//            for (i = 1; i <= 10; i++)
+//            {
+//                Console.WriteLine($"{num} x {i} = {num * i}");
+//            }
+//        }
+//        static int Factorial(int num) // accept parameter and return type int
+//        {
+//            int f = 1, i;
+//            for (i = num; i >= 1; i--)
+//            {
+//                Console.Write($"{i}x");
+//                f = f * i;
+//            }
+//            return f;
+//        }
+//        static double GetPi() // does not accept parameter but return type double
+//        {
+//            return 3.14;
+//        }
+//    }
+//}
+
+///*
+// Function - 
+//        1. Block of Code under a Name
+//        2. Function sign agreement that is just call me and use me 
+//        3. Reusable code 
+//        4. Function has 4 types 
+//            4.1. No parameter and no return type (does not accept and does not return anything)
+//                 We define a function with void return type and enpty parenthesis 
+                
+//            2. Parameter but no return type
+//            3. No parameter but return type
+//            4. Parameter and return type
+ 
+// */
